4291de6f8db2c101085fe5a72907fa1757759599
J Lakshmi Vivek <jlakshmivivek17@gmail.com>
2025-02-10 22:10:59 -0500

    first Commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <p className="text-2xl">Hi this is the test</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
